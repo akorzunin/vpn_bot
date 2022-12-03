a142d4c8f0c7ed2724e200c33d0a0bf4daec45e2
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-12-03 15:47:38 +0300

    fix: asyncio get_event_loop deprication warning

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from src.fastapi_app.fastapi_app import app
 from src.fastapi_app.uvicrorn_confgis import uvicorn_conf
 from src.tasks.scheduler import scheduler
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     # create fastapi task
-    config = uvicorn.Config(**uvicorn_conf, loop=loop)
+    config = uvicorn.Config(**uvicorn_conf, loop="asyncio")
     server = uvicorn.Server(config)
     loop.create_task(server.serve())
     # start scheduler
@@ -20,13 +21,16 @@ if __name__ == '__main__':
     # tasks
 
     # setup debug console to work w/ awaitables
-    if bool(eval(os.getenv('DEBUG'))):  # type: ignore
+    if bool(eval(os.getenv("DEBUG"))):  # type: ignore
         # import nest_asyncio
-        # nest_asyncio.apply(loop) 
+        # nest_asyncio.apply(loop)
         # loop.set_debug(1)
         ...
 
     # message_handler.setup(aiogram_app.dp)
-    aiogram_app.executor.start_polling(aiogram_app.dp, skip_updates=True)
+    aiogram_app.executor.start_polling(
+        aiogram_app.dp,
+        skip_updates=True,
+    )
     if 1:
         loop.run_forever()
