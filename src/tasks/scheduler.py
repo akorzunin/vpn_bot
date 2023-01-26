5d018295ef8de7b788e3d78973c1f7b56159e479
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2023-01-26 22:23:44 +0300

    fix(tasks): better envent handling and logging set up jobs #14

--- src/tasks/scheduler.py
+++ src/tasks/scheduler.py
@@ -1,3 +1,75 @@
+import asyncio
+from datetime import UTC
+import logging
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.events import (
+    EVENT_JOB_EXECUTED,
+    EVENT_JOB_ERROR,
+    EVENT_JOB_MISSED,
+    EVENT_JOB_REMOVED,
+)
 
-scheduler = AsyncIOScheduler()
+from src.tasks.task_configs import NO_REVIVE_PERIOD
+from src.db import crud
+
+scheduler = AsyncIOScheduler(
+    job_defaults={
+        "coalesce": True,  # run job after its missed in down time
+        "max_instances": 1,  # run only one instance of job
+        "misfire_grace_time": int(
+            NO_REVIVE_PERIOD.total_seconds()
+        ),  # time to pick up missed job
+    },
+    timezone=UTC,
+)
+
+
+def call_coroutine(coro):
+    def wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        loop.create_task(coro(*args, **kwargs))
+
+    return wrapper
+
+
+def get_user_id_from_job_id(job_id: str) -> int:
+    return int(job_id.split("_")[1])
+
+
+def exeption_listener(event):
+    if event.exception:
+        logging.error(f"{event.exception} The job crashed")
+
+
+@call_coroutine
+async def job_missed_listener(event):
+    user_id = get_user_id_from_job_id(event.job_id)
+    user = await crud.get_user_by_telegram_id(user_id)
+    logging.warning(
+        f"Job {event.job_id} {user.balance} {user.next_payment} missed"
+    )
+
+
+@call_coroutine
+async def job_removed_listener(event):
+    user_id = get_user_id_from_job_id(event.job_id)
+    user = await crud.get_user_by_telegram_id(user_id)
+    logging.warning(
+        f"Job {event.job_id} {user.balance} {user.next_payment} removed"
+    )
+
+
+scheduler.add_listener(
+    exeption_listener,
+    EVENT_JOB_EXECUTED | EVENT_JOB_ERROR,
+)
+
+scheduler.add_listener(
+    job_missed_listener,
+    EVENT_JOB_MISSED,
+)
+
+scheduler.add_listener(
+    job_removed_listener,
+    EVENT_JOB_REMOVED,
+)
