b8b019fbf79ffadc9a95c74963b3097487d1426f
akorzunin <akorzunin123@gmail.com>
2023-03-27 22:56:28 +0300

    feat(locales): translate user_commands #27

--- src/aiogram_app/user_commands.py
+++ src/aiogram_app/user_commands.py
@@ -16,20 +16,22 @@ from src.db.schemas import VpnConfig
 from src.fastapi_app import user_routes, admin_routes
 from src.fastapi_app import pivpn_wrapper as pivpn
 from src.utils.errors.pivpn_errors import PiVpnException
+from src.locales import get_locale
 
 
 @dp.message_handler(commands=["user", "me"])
 async def t_me(message: types.Message):
     """get information about current user from db"""
+    _ = get_locale(message)
     # get user info
     user = await crud.find_user_by_telegram_id(message.from_user.id)
     if not user:
         await message.answer(
-            "User not found, u can create a new one with /start"
+            _("User not found, u can create a new one with /start")
         )
     else:
         # format user in human readable format
-        user_str = prepare_user_str(user)
+        user_str = prepare_user_str(user, _)
         await message.answer(user_str)
 
 
@@ -38,12 +40,13 @@ async def delete_user(message: types.Message):
     """delete user from db"""
     # TODO prompt for confirmation
     # call fastapi to delete user
+    _ = get_locale(message)
     user = await crud.get_user_by_telegram_id(message.from_user.id)
     if user.conf_files:
         for config in user.conf_files:
             try:
                 _config = pivpn.delete_vpn_config(config.user_name)
-                logging.info(f"Vpn config deleted: {_config}")
+                logging.info(f"{_('Vpn config deleted')}: {_config}")
             except PiVpnException as e:
                 logging.error(e)
     response = await user_routes.delete_user(
@@ -52,20 +55,20 @@ async def delete_user(message: types.Message):
     )
     if response.status_code == 400:
         await message.answer(
-            "User not found, u can create a new one with /start"
+            _("User not found, u can create a new one with /start")
         )
     elif response.status_code == 200:
-        await message.answer("User deleted")
+        await message.answer(_("User deleted"))
 
 
 @dp.message_handler(commands=["remove_config", "del_config"])
 async def remove_config(message: types.Message):
     """remove vpn config from user"""
-
+    _ = get_locale(message)
     # get arg as user_name
     user_name = message.get_args()
     if not user_name:
-        await message.answer("Please provide config user name")
+        await message.answer(_("Please provide config user name"))
         return
     # call fastapi to delete user
     response = await user_routes.remove_vpn_config(
@@ -74,21 +77,23 @@ async def remove_config(message: types.Message):
         api_credentials,
     )
     if response.status_code == 400:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
     elif response.status_code == 200:
-        await message.answer("VPN config removed")
+        await message.answer(_("VPN config removed"))
 
 
 @dp.message_handler(commands=["add_config", "add_vpn_config"])
 async def add_config(message: types.Message):
     """add vpn config to user"""
-
+    _ = get_locale(message)
     # get arg as user_name
     user_name = message.get_args()
     if not user_name:
         await message.answer(
-            "Please provide config user name after command\n"
-            "/add_config <user_name>"
+            _(
+                "Please provide config user name after command\n"
+                "/add_config <user_name>"
+            )
         )
         return
 
@@ -107,10 +112,10 @@ async def add_config(message: types.Message):
         api_credentials,
     )
     if response.status_code == 400:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
     elif response.status_code == 200:
         await message.answer(
-            escape_markdown(format_vpn_config(data, user_name)),
+            escape_markdown(format_vpn_config(data, user_name, _)),
             parse_mode="Markdown",
         )
 
@@ -118,16 +123,17 @@ async def add_config(message: types.Message):
 @dp.message_handler(commands=["list_configs", "list_vpn_configs"])
 async def list_configs(message: types.Message):
     """list vpn configs of user"""
+    _ = get_locale(message)
     # get user from fastapi
     user = await user_routes.get_user_by_id(
         message.from_user.id,
         api_credentials,
     )
     if not user:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
         return
     if not user.conf_files:
-        await message.answer("No configs found")
+        await message.answer(_("No configs found"))
         return
     # conf_files paths from user object
     conf_data: list[tuple[str, str]] = [
@@ -142,7 +148,7 @@ async def list_configs(message: types.Message):
     ]
 
     await message.answer(
-        escape_markdown(format_many_vpn_configs(conf_data)),
+        escape_markdown(format_many_vpn_configs(conf_data, _)),
         parse_mode="Markdown",
     )
 
@@ -150,21 +156,23 @@ async def list_configs(message: types.Message):
 @dp.message_handler(commands=["balance", "get_balance"])
 async def get_balance(message: types.Message):
     """get balance of user"""
+    _ = get_locale(message)
     user = await crud.find_user_by_telegram_id(message.from_user.id)
     if user:
         await message.answer(
-            f"Balance: {user.balance}\nNext payment: {user.next_payment}"
+            f"{_('Balance')}: {user.balance}\n{_('Next payment')}: {user.next_payment}"
         )
         return
-    await message.answer("User not found")
+    await message.answer(_("User not found"))
 
 
 @dp.message_handler(commands=["redeem", "redeem_code"])
 async def redeem_code(message: types.Message):
     """redeem code"""
+    _ = get_locale(message)
     code = message.get_args()
     if not code:
-        await message.answer("Please provide code\n/redeem <code>")
+        await message.answer(_("Please provide code\n/redeem <code>"))
         return
     response = await user_routes.redeem_code(
         message.from_user.id,
@@ -172,32 +180,34 @@ async def redeem_code(message: types.Message):
         credentials=api_credentials,
     )
     if response.status_code == 400:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
     elif response.status_code == 200:
-        await message.answer("Code redeemed")
+        await message.answer(_("Code redeemed"))
 
 
 @dp.message_handler(commands=["activate", "activate_subscription"])
 async def activate_subscription(message: types.Message):
     """activate subscription"""
+    _ = get_locale(message)
     response = await user_routes.activate_subscription(
         message.from_user.id,
         credentials=api_credentials,
     )
     if response.status_code == 400:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
     elif response.status_code == 200:
-        await message.answer("Subscription activated")
+        await message.answer(_("Subscription activated"))
 
 
 @dp.message_handler(commands=["deactivate", "deactivate_subscription"])
 async def deactivate_subscription(message: types.Message):
     """deactivate subscription"""
+    _ = get_locale(message)
     response = await user_routes.deactivate_subscription(
         message.from_user.id,
         credentials=api_credentials,
     )
     if response.status_code == 400:
-        await message.answer("User not found")
+        await message.answer(_("User not found"))
     elif response.status_code == 200:
-        await message.answer("Subscription deactivated")
+        await message.answer(_("Subscription deactivated"))
---
+++ src/locales.py
@@ -0,0 +1,19 @@
+import gettext
+from typing import Callable
+from aiogram.types import Message
+
+path = "locale"
+gettext.bindtextdomain("messages", path)
+gettext.textdomain("messages")
+gettext.install(
+    "messages",
+    path,
+)
+
+en = gettext.translation("messages", localedir=path, languages=["en"])
+ru = gettext.translation("messages", localedir=path, languages=["ru"])
+_ = gettext.gettext
+
+
+def get_locale(message: Message) -> Callable:
+    return ru.gettext if message.from_user.language_code == "ru" else en.gettext
