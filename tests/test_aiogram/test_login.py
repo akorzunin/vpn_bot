0e4158d4c720560ee75cd4e35a0c794fb10f741a
akorzunin <akorzunin123@gmail.com>
2023-03-29 23:48:50 +0300

    fix(tests): fix aiorgam mock message fields

--- tests/test_aiogram/test_login.py
+++ tests/test_aiogram/test_login.py
@@ -15,16 +15,16 @@ from src.aiogram_app.aiogram_app import dp
 async def test_login():
     """Test login"""
     message = types.Message()
-    with open("./tests/mock_data/mock_login/login_message.pkl", "rb") as f:
-        message = dill.load(f)
+    message.from_user = mock.Mock()
     message.answer = mock.AsyncMock()
+    message.reply = mock.AsyncMock()
     with open(
         "./tests/mock_data/mock_login/login_inline_buttons.pkl", "rb"
     ) as f:
         keyboard_markup: InlineKeyboardMarkup = dill.load(f)
     await login_commands.login_start(message)
-    keyboard_markup.inline_keyboard[0][0].text = "Login w/ username"
-    keyboard_markup.inline_keyboard[1][0].text = "Login w/ invitation code"
+    keyboard_markup.inline_keyboard[0][0].text = "Login with username"
+    keyboard_markup.inline_keyboard[1][0].text = "Login with invitation code"
     keyboard_markup.inline_keyboard[2][0].text = "Cancel"
     message.answer.assert_called_once_with(
         "Select a method to login. "
